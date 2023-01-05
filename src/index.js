86a0192e0682a99490ccb6bfc8ed2a37221ebf59
ElenaBagriy <elena.bagriy1989@gmail.com>
2023-01-05 14:58:16 +0200

    update task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchcountries';
+import debounce from 'lodash.debounce'
+import { Notify } from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector('input#search-box');
+const countryList = document.querySelector('ul.country-list');
+
+Notify.init({
+    width: '400px',
+    position: 'center-top',
+});
+
+input.addEventListener('input', debounce(onInputFullfill, DEBOUNCE_DELAY));
+
+function onInputFullfill(e) {
+    const { value } = e.target;
+
+    if (!value) {
+        return;
+    };
+
+    fetchCountries(value.trim())
+        .then(countries => createMarkup(countries))
+        .catch(onErrorCatch);
+}
+
+function createMarkup(countries) {
+    countryList.innerHTML = '';
+
+    if (!input.value) {
+        return countryList.innerHTML = '';
+    }
+
+    if (countries.length >= 10) {
+        return Notify.info('Too many matches found. Please enter a more specific name.');
+    }
+
+    countries.map(({ name, capital, population, flags, languages }) => {
+        
+        const shortCard = `<li class="js-country-item">
+        <image src="${flags.svg}" width="30px" height="20px">
+        <span>${name.official}</span>
+        </li>`;
+
+        const fullCard = `<li class="js-country-item">
+        <ul class="js-fullcard-list">
+        <div class="js-country-header-wrapper">
+        <image src="${flags.svg}" width="30px" height="20px">
+        <h2 class="js-country-item__header">${name.official}</h2>
+        </div>
+        <li class="js-fullcard-item"><span class="js-fullcard-item-bold">Capital: </span>${capital}</li>
+        <li class="js-fullcard-item"><span class="js-fullcard-item-bold">Population: </span>${population}</li>
+        <li class="js-fullcard-item"><span class="js-fullcard-item-bold">Languages: </span>${Object.values(languages).join(', ')}</li>
+        </ul>
+        </li>`;
+
+    if (countries.length >= 2 && countries.length < 10) {
+        return countryList.insertAdjacentHTML('beforeend', shortCard);
+    }
+        countryList.insertAdjacentHTML('beforeend', fullCard);
+    })
+}
+
+function onErrorCatch(error) {
+    return Notify.failure('Oops, there is no country with that name');
+}
